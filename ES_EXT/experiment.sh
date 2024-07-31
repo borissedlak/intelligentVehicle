6c5203583d0c105297c265e0e5050ba5fc6c7094
Boris Sedlak <boris.sedlak@dsg.tuwien.ac.at>
2024-07-31 08:59:23 +0800

    config for NX

--- ES_EXT/experiment.sh
+++ ES_EXT/experiment.sh
@@ -2,8 +2,8 @@
 
 #chmod +x run_python_script.sh
 export PYTHONPATH=~/development/intelligentVehicle
-export INITIAL_TRAINING=30
-export EXPERIMENT_DURATION=300 # 600 = 10 min
+export INITIAL_TRAINING=40
+export EXPERIMENT_DURATION=600 # 600 = 10 min
 export SHOW_IMG="False" # 600 = 10 min
 
 mkdir mkdir ~/development/intelligentVehicle/ES_EXT/models/backup
@@ -17,10 +17,12 @@ if [ "$DEVICE_NAME" = "NX" ] || [ "$DEVICE_NAME" = "AGX" ]; then
     sudo nvpmodel -m 0
     export POWER_MODE="MAX"
 fi
+	
+#SERVICE_NAME="CV" python3 ~/development/intelligentVehicle/ES_EXT/agent.py
+#SERVICE_NAME="QR" python3 ~/development/intelligentVehicle/ES_EXT/agent.py
+#SERVICE_NAME="LI" python3 ~/development/intelligentVehicle/ES_EXT/agent.py
 
-SERVICE_NAME="CV" python3 ~/development/intelligentVehicle/ES_EXT/agent.py
-SERVICE_NAME="QR" python3 ~/development/intelligentVehicle/ES_EXT/agent.py
-SERVICE_NAME="LI" python3 ~/development/intelligentVehicle/ES_EXT/agent.py
+python3 ~/development/intelligentVehicle/ES_EXT/models/model_trainer.py
 
 if [ "$DEVICE_NAME" = "NX" ] || [ "$DEVICE_NAME" = "AGX" ]; then
     sudo nvpmodel -m 3
